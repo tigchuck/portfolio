f78ca826f8391b5120253903db6f05d509213b1f
Charlie Tighe <tigchuck@gmail.com>
2023-09-05 22:18:15 -0400

    Worked on Home page.

--- src/components/FunName.js
+++ src/components/FunName.js
@@ -3,80 +3,160 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Slide from '@mui/material/Slide';
 import Grow from '@mui/material/Grow';
-import Fade from '@mui/material/Fade';
+// import Fade from '@mui/material/Fade';
 
 
 
 function FunName() {
 
-    const [slide, setSlide] = useState(false);
+    const [slide1, setSlide1] = useState(false);
+    const [slide2, setSlide2] = useState(false);
+    const [slide3, setSlide3] = useState(false);
     const [grow, setGrow] = useState(false);
-    const [fade, setFade] = useState(false);
 
     useEffect(() => {
-        setFade(true);
+        setGrow(true);
     }, [])
 
     return (
-        <Grid container>
-            <Fade 
-                in={fade} 
-                timeout={{
-                    enter: 1000,
-                }}
-                onEntered={() => {
-                    setSlide(true);
-                }}
-            >
-                <Typography
-                    sx={{
-                        fontFamily: "Abel",
-                        fontSize: "32px",
-                        lineHeight: "48px",
-                        fontWeight: "900",
-                        '&:hover': {
-                            textShadow: "0px 0px 5px red"
-                        },
-                    }}
+        <Grid 
+            container
+            flexDirection="column"
+            spacing={6}
+            sx={{
+                // maxWidth: { xs: "0.8", md: "0.4" },
+            }}
+        >
+            <Grid item>
+                <Grid 
+                    container 
+                    direction="row"
+                    spacing={3}
                 >
-                
-                    Hi, I'm 
-                </Typography>
-            </Fade>
-            <Slide
-                direction="down"
-                in={slide}
-                timeout={{
-                    enter: 1000,
-                }}
-                onEntered={() => {
-                    setGrow(true);
-                }}
-            >
-                <Typography
-                    sx={{
-                        fontFamily: "Abel",
-                        fontSize: "32px",
-                        lineHeight: "48px",
-                        fontWeight: "900",
-                        '&:hover': {
-                            textShadow: "0px 0px 5px red"
-                        },
-                    }}
-                >
-                    Charlie
-                </Typography>
-            </Slide>
-            <Grow
-                in={grow} 
-                timeout={{
-                    enter: 1000,
-                }}
-            >
-                <Typography>
-                    I'm a graduate student at Georgia Tech pursuing an M.S. in Computer Science. I have a B.S. in Biomedical Engineering from Ohio State and am an experienced Product Design Engineer and Product Manager.
-                </Typography>
-            </Grow>
+                    <Grid item>
+                        <Grow 
+                            in={grow} 
+                            timeout={{
+                                enter: 1000,
+                            }}
+                            onEntered={() => {
+                                setSlide1(true);
+                            }}
+                        >
+                            <Typography
+                                sx={{
+                                    fontFamily: "Abel",
+                                    fontSize: "64px",
+                                    lineHeight: "48px",
+                                    fontWeight: "900",
+                                    lineWeight: "bold",
+                                }}
+                            >
+                            
+                                Hi, I'm
+                            </Typography>
+                        </Grow>
+
+                    </Grid>
+                    <Grid item>
+                        <Grow 
+                            in={grow} 
+                            timeout={{
+                                enter: 1000,
+                            }}
+                            onEntered={() => {
+                                setSlide1(true);
+                            }}
+                        >
+                            <Typography
+                                sx={{
+                                    fontFamily: "Abel",
+                                    fontSize: "80px",
+                                    lineHeight: "48px",
+                                    fontWeight: "900",
+                                    lineWeight: "extra-bold",
+                                }}
+                            >
+                            
+                                Charlie
+                            </Typography>
+                        </Grow>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item>
+                <Grid container direction="column" spacing={2}>
+                    <Grid item>
+                        <Slide
+                            direction="right"
+                            in={slide1}
+                            timeout={{
+                                enter: 1000,
+                            }}
+                            onEntered={() => {
+                                setSlide2(true);
+                            }}
+                        >
+                            <Typography
+                                sx={{
+                                    fontFamily: "Abel",
+                                    fontSize: "32px",
+                                    lineHeight: "48px",
+                                    fontWeight: "900",
+                                    lineWeight: "bold",
+                                }}
+                            >
+                                I'm a graduate student at Georgia Tech pursuing an M.S. in Computer Science.
+                            </Typography>
+                        </Slide>
+                    </Grid>
+                    <Grid item>
+                        <Slide
+                            direction="right"
+                            in={slide2} 
+                            timeout={{
+                                enter: 1000,
+                            }}
+                            onEntered={() => {
+                                setSlide3(true);
+                            }}
+                        >
+                            <Typography
+                                sx={{
+                                    fontFamily: "Abel",
+                                    fontSize: "32px",
+                                    lineHeight: "48px",
+                                    fontWeight: "900",
+                                    lineWeight: "bold",
+                                }}
+                            >
+                                I have a B.S. in Biomedical Engineering from Ohio State.
+                            </Typography>
+                        </Slide>
+                    </Grid>
+                    <Grid item>
+                        <Slide
+                            direction="right"
+                            in={slide3} 
+                            timeout={{
+                                enter: 1000,
+                            }}
+                        >
+                            <Typography
+                                sx={{
+                                    fontFamily: "Abel",
+                                    fontSize: "32px",
+                                    lineHeight: "48px",
+                                    fontWeight: "900",
+                                    lineWeight: "bold",
+                                }}
+                            >
+                                I'm an experienced Product Design Engineer and Product Manager.
+                            </Typography>
+                        </Slide>
+                    </Grid>
+                </Grid>
+            </Grid>
         </Grid>
     );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,21 +1,82 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
-// import Typography from '@mui/material/Typography';
-// import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import CardMedia from '@mui/material/CardMedia';
+import Paper from '@mui/material/Paper';
+import Fab from '@mui/material/Fab';
 import FunName from "../FunName";
+import charlie from "../../pictures/CharliePicture.jpeg";
 
 
 function Home() {
     return (
         <Grid
             container
-            flexDirection="row"
-            justifyContent="space-around"
+            flexDirection="column"
         >
             <Grid item>
-                <FunName />
+                <Grid
+                    container
+                    flexDirection="row"
+                    justifyContent="space-around"
+                    alignItems="center"
+                    sx={{
+                        padding: "50px"
+                    }}
+                >
+                    <Grid item>
+                        <FunName />
+                    </Grid>
+                    <Grid item sx={{borderRadius: "25px"}}>
+                        <Paper img={charlie} elevation={10} sx={{borderRadius: "50%"}}>
+                            <CardMedia component="img" image={charlie} sx={{borderRadius: "50%"}} />
+                        </Paper>
+                    </Grid>
+                </Grid>
             </Grid>
-            <Grid item>Also Home</Grid>
+            <Grid item>
+                <Grid 
+                    contianer
+                    flexDirection="column"
+                >
+                    <Grid item>
+                        <Fab
+                            variant="extended"
+                            href="/contact"
+                            sx={{
+                                fontFamily: "Abel",
+                                fontSize: "16px",
+                                margin: "32px auto",
+                                display: "flex",
+                                width: "0.25",
+                                minWidth: "250px"
+                            }}
+                        >
+                            Contact Me!
+                        </Fab>
+                    </Grid>
+                    <Grid item>
+                        <Grid container flexDirection="row">
+                            <Grid item>
+                                <IconButton href="https://github.com/tigchuck">
+                                    <GitHubIcon 
+                                        fontSize="large" 
+                                    />
+                                </IconButton>
+                            </Grid>
+                            <Grid item>
+                                <IconButton href="https://www.linkedin.com/in/charlie-tighe-2b0062136/">
+                                    <LinkedInIcon 
+                                        fontSize="large" 
+                                    />
+                                </IconButton>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         </Grid>
     );
 }
